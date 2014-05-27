7ccba8024220a4a44b5d0d7a2da832176abbfc69
h41xy <urs.oberdorf@autistici.org>
2014-05-27 10:45:31 +0000

    aufgabe calc hinzugefuegt

---
+++ ueb04/calc1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Expression
+
+if [ $# -ne 2 ]
+then
+	echo "Brauche 2 Argumente."
+	exit 1
+fi
+
+result=`expr \( $1 \* $1 \* $1 \* $1 \* $1 - $1 \) / 30 + $2`
+
+if [ $? -ne 0 ]
+then
+	echo "expr resultet in an Error"
+	exit $?
+fi
+
+echo "$result"
+exit 0
